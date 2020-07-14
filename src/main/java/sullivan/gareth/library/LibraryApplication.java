e7cdf182b02ac0a7e52585616898306987e297b7
Gareth Sullivan <sullivan.gareth@gmail.com>
2020-07-14 19:46:59 +0100

    Initial repository checkin
    Created using Spring Initializr

---
+++ src/main/java/sullivan/gareth/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package sullivan.gareth.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
